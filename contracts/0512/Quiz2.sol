686bc41ab285591e3a0c540c2efe7e378f03ce37
PPisland <a1365gkqls@gmail.com>
2023-05-15 10:39:03 +0900

    0515-quiz2

--- contracts/0512/Quiz2.sol
+++ contracts/0512/Quiz2.sol
@@ -44,35 +44,49 @@ contract Solution {
     // * 회원가입시 이미 존재한 아이디 체크 여부 기능 - 이미 있는 아이디라면 회원가입 중지
     // * 비밀번호 5회 이상 오류시 경고 메세지 기능 - 비밀번호 시도 회수가 5회되면 경고 메세지 반환
     // * 회원탈퇴 기능 - 회원이 자신의 ID와 PW를 넣고 회원탈퇴 기능을 실행하면 관련 정보 삭제 
+    struct User{
+        bytes32 hash;
+        uint attempts;
+    }
 
+    mapping(string=> User) ID_PW;
 
-    mapping(string=>bytes32) ID_PW;
+    function getHash(string memory _ID, string memory _PW) public pure returns(bytes32) {
+        return keccak256(abi.encodePacked(_ID, _PW));
+    }
 
      // * 로그인 기능 - ID, PW를 넣으면 로그인 여부를 알려주는 기능
-    function Login(string memory _ID, string memory _PW) public view returns (bool){
+    function Login(string memory _ID, string memory _PW) public  returns (bool){
         // bytes32 _password = keccak256(abi.encodePacked(_ID,_PW));
-        return ID_PW[_ID] == keccak256(abi.encodePacked(_ID,_PW));
+        require(ID_PW[_ID].attempts <5, "Too much attempts");
+        if(ID_PW[_ID].hash == keccak256(abi.encodePacked(_ID, _PW))){
+            ID_PW[_ID].attempts = 0;
+            return true;
+        }
+        else{
+            ID_PW[_ID].attempts++;
+            return false;
+        }
+        // return ID_PW[_ID] == keccak256(abi.encodePacked(_ID,_PW));
     }
     function Login2(string memory _ID) public view returns(bytes32) {
-        return ID_PW[_ID];
+        return ID_PW[_ID].hash;
     }
      // * 회원가입 기능 - 새롭게 회원가입할 수 있는 기능
     function SignIn(string memory _ID, string memory _PW) public {
      // * 회원가입시 이미 존재한 아이디 체크 여부 기능 - 이미 있는 아이디라면 회원가입 중지
-        require(ID_PW[_ID] ==  0x0000000000000000000000000000000000000000000000000000000000000000, "provided ID is already being used");
-        // require(ID_PW[_ID] ==  "", "provided ID is already being used"); <- 이렇게도 가능!
+        require(ID_PW[_ID].hash ==  0x0000000000000000000000000000000000000000000000000000000000000000, "provided ID is already being used");
+        // require(ID_PW[_ID].hash ==  "", "provided ID is already being used"); <- 이렇게도 가능!
 
-        ID_PW[_ID] = keccak256(abi.encodePacked(_ID,_PW));
+        ID_PW[_ID].hash = keccak256(abi.encodePacked(_ID,_PW));
     }
     // * 회원탈퇴 기능 - 회원이 자신의 ID와 PW를 넣고 회원탈퇴 기능을 실행하면 관련 정보 삭제 
     function singOut(string memory _ID, string memory _PW) public {
-        // require(ID_PW[_ID] == keccak256(abi.encodePacked(_ID,_PW)));
-        require(Login(_ID, _PW) == true);
+        // require(ID_PW[_ID].hash == keccak256(abi.encodePacked(_ID,_PW)));
+        // require(Login(_ID, _PW) == true); Login 함수를 쓰면 위에서 public함수이기 때문에 돈을 또 쓰므로 위에 함수를 씀
+        require (ID_PW[_ID].hash == getHash(_ID, _PW));
         delete ID_PW[_ID];
-    }
-
-
-    
+    }    
 }
 
 contract REQUIRE {
